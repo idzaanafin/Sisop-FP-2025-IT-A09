51d0ae755c5a4daf286ae0eb6f6fa4f9f9e81246
idzaanafin <ahmadidanafin@gmail.com>
2025-07-02 02:27:43 +0700

    update

--- src/readonly_fs.c
+++ src/readonly_fs.c
@@ -1,3 +1,296 @@
+// #define FUSE_USE_VERSION 26
+// #define _FILE_OFFSET_BITS 64
+
+// #include <fuse.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <fcntl.h>
+// #include <unistd.h>
+// #include <dirent.h>
+// #include <sys/stat.h>
+
+// static const char *source_dir = "/home/idzoyy/Sisop-FP-2025-IT-A09/source_dir";
+
+// static void fullpath(char fpath[PATH_MAX], const char *path) {
+//     sprintf(fpath, "%s%s", source_dir, path);
+// }
+
+// static int xmp_getattr(const char *path, struct stat *stbuf) {
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int res = lstat(fpath, stbuf);
+//     if (res == -1)
+//         return -errno;
+//     return 0;
+// }
+
+// static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+//                        off_t offset, struct fuse_file_info *fi) {
+//     DIR *dp;
+//     struct dirent *de;
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+
+//     dp = opendir(fpath);
+//     if (dp == NULL)
+//         return -errno;
+
+//     while ((de = readdir(dp)) != NULL) {
+//         struct stat st;
+//         memset(&st, 0, sizeof(st));
+//         st.st_ino = de->d_ino;
+//         st.st_mode = de->d_type << 12;
+//         if (filler(buf, de->d_name, &st, 0))
+//             break;
+//     }
+//     closedir(dp);
+//     return 0;
+// }
+
+// static int xmp_open(const char *path, struct fuse_file_info *fi) {
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int fd = open(fpath, O_RDONLY);
+//     if (fd == -1)
+//         return -errno;
+//     close(fd);
+//     return 0;
+// }
+
+// static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+//                     struct fuse_file_info *fi) {
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int fd = open(fpath, O_RDONLY);
+//     if (fd == -1)
+//         return -errno;
+
+//     int res = pread(fd, buf, size, offset);
+//     if (res == -1)
+//         res = -errno;
+
+//     close(fd);
+//     return res;
+// }
+
+// static int xmp_writeblock() {
+//     fprintf(stderr, "Gabisa ya huhu\n");
+//     return -EROFS;
+// }
+
+// static struct fuse_operations xmp_oper = {
+//     .getattr = xmp_getattr,
+//     .readdir = xmp_readdir,
+//     .open    = xmp_open,
+//     .read    = xmp_read,
+//     .mkdir   = (void*) xmp_writeblock,
+//     .unlink  = (void*) xmp_writeblock,
+//     .rmdir   = (void*) xmp_writeblock,
+//     .rename  = (void*) xmp_writeblock,
+//     .write   = (void*) xmp_writeblock,
+//     .create  = (void*) xmp_writeblock,
+// };
+
+// int main(int argc, char *argv[]) {
+//     return fuse_main(argc, argv, &xmp_oper, NULL);
+// }
+
+
+
+// #define FUSE_USE_VERSION 26
+// #define _FILE_OFFSET_BITS 64
+
+// #include <fuse.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <errno.h>
+// #include <fcntl.h>
+// #include <unistd.h>
+// #include <dirent.h>
+// #include <sys/stat.h>
+// #include <stdlib.h>
+
+// // static const char *source_dir = "/Users/dina-r/Desktop/finalssp/source_dir";
+// static const char *source_dir = "/home/idzoyy/Sisop-FP-2025-IT-A09/source_dir";
+// static int is_admin = 0; // 0 = user (read-only), 1 = admin (full access)
+
+// static void fullpath(char fpath[PATH_MAX], const char *path) {
+//     sprintf(fpath, "%s%s", source_dir, path);
+// }
+
+// static int xmp_getattr(const char *path, struct stat *stbuf) {
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int res = lstat(fpath, stbuf);
+//     if (res == -1)
+//         return -errno;
+//     return 0;
+// }
+
+// static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+//                        off_t offset, struct fuse_file_info *fi) {
+//     DIR *dp;
+//     struct dirent *de;
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+
+//     dp = opendir(fpath);
+//     if (dp == NULL)
+//         return -errno;
+
+//     while ((de = readdir(dp)) != NULL) {
+//         struct stat st;
+//         memset(&st, 0, sizeof(st));
+//         st.st_ino = de->d_ino;
+//         st.st_mode = de->d_type << 12;
+//         if (filler(buf, de->d_name, &st, 0))
+//             break;
+//     }
+//     closedir(dp);
+//     return 0;
+// }
+
+// static int xmp_open(const char *path, struct fuse_file_info *fi) {
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int fd = open(fpath, O_RDONLY);
+//     if (fd == -1)
+//         return -errno;
+//     close(fd);
+//     return 0;
+// }
+
+// static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+//                     struct fuse_file_info *fi) {
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int fd = open(fpath, O_RDONLY);
+//     if (fd == -1)
+//         return -errno;
+
+//     int res = pread(fd, buf, size, offset);
+//     if (res == -1)
+//         res = -errno;
+
+//     close(fd);
+//     return res;
+// }
+
+// static int xmp_write(const char *path, const char *buf, size_t size, off_t offset,
+//                      struct fuse_file_info *fi) {
+//     if (!is_admin) return -EROFS;
+
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int fd = open(fpath, O_WRONLY);
+//     if (fd == -1)
+//         return -errno;
+
+//     int res = pwrite(fd, buf, size, offset);
+//     if (res == -1)
+//         res = -errno;
+
+//     close(fd);
+//     return res;
+// }
+
+// static int xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
+//     if (!is_admin) return -EROFS;
+
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int fd = creat(fpath, mode);
+//     if (fd == -1)
+//         return -errno;
+
+//     close(fd);
+//     return 0;
+// }
+
+// static int xmp_mkdir(const char *path, mode_t mode) {
+//     if (!is_admin) return -EROFS;
+
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int res = mkdir(fpath, mode);
+//     if (res == -1)
+//         return -errno;
+//     return 0;
+// }
+
+// static int xmp_unlink(const char *path) {
+//     if (!is_admin) return -EROFS;
+
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int res = unlink(fpath);
+//     if (res == -1)
+//         return -errno;
+//     return 0;
+// }
+
+// static int xmp_rmdir(const char *path) {
+//     if (!is_admin) return -EROFS;
+
+//     char fpath[PATH_MAX];
+//     fullpath(fpath, path);
+//     int res = rmdir(fpath);
+//     if (res == -1)
+//         return -errno;
+//     return 0;
+// }
+
+// static int xmp_rename(const char *from, const char *to) {
+//     if (!is_admin) return -EROFS;
+
+//     char ffrom[PATH_MAX];
+//     char fto[PATH_MAX];
+//     fullpath(ffrom, from);
+//     fullpath(fto, to);
+//     int res = rename(ffrom, fto);
+//     if (res == -1)
+//         return -errno;
+//     return 0;
+// }
+
+// static struct fuse_operations xmp_oper = {
+//     .getattr = xmp_getattr,
+//     .readdir = xmp_readdir,
+//     .open    = xmp_open,
+//     .read    = xmp_read,
+//     .write   = xmp_write,
+//     .create  = xmp_create,
+//     .mkdir   = xmp_mkdir,
+//     .unlink  = xmp_unlink,
+//     .rmdir   = xmp_rmdir,
+//     .rename  = xmp_rename,
+// };
+
+// int main(int argc, char *argv[]) {
+//     char role[10];
+//     char password[100];
+
+//     printf("Masukkan mode akses (admin/user): ");
+//     scanf("%9s", role);
+
+//     if (strcmp(role, "admin") == 0) {
+//         printf("Masukkan password: ");
+//         scanf("%99s", password);
+//         if (strcmp(password, "sisop2025") == 0) {
+//             printf("Akses admin diberikan.\n");
+//             is_admin = 1;
+//         } else {
+//             printf("Password salah! Mode read-only aktif.\n");
+//         }
+//     } else {
+//         printf("Mode user aktif. Hanya read-only.\n");
+//     }
+
+//     return fuse_main(argc, argv, &xmp_oper, NULL);
+// }
+
+
 #define FUSE_USE_VERSION 26
 #define _FILE_OFFSET_BITS 64
 
@@ -9,8 +302,10 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
-static const char *source_dir = "/Users/dina-r/Desktop/finalssp/source_dir";
+static const char *source_dir = "/home/idzoyy/Sisop-FP-2025-IT-A09/source_dir";
+static int is_admin = 0; // 0 = user (read-only), 1 = admin (full access)
 
 static void fullpath(char fpath[PATH_MAX], const char *path) {
     sprintf(fpath, "%s%s", source_dir, path);
@@ -74,9 +369,81 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
     return res;
 }
 
-static int xmp_writeblock() {
-    fprintf(stderr, "Gabisa ya huhu\n");
-    return -EROFS;
+static int xmp_write(const char *path, const char *buf, size_t size, off_t offset,
+                     struct fuse_file_info *fi) {
+    if (!is_admin) return -EROFS;
+
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+    int fd = open(fpath, O_WRONLY);
+    if (fd == -1)
+        return -errno;
+
+    int res = pwrite(fd, buf, size, offset);
+    if (res == -1)
+        res = -errno;
+
+    close(fd);
+    return res;
+}
+
+static int xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi) {
+    if (!is_admin) return -EROFS;
+
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+    int fd = creat(fpath, mode);
+    if (fd == -1)
+        return -errno;
+
+    close(fd);
+    return 0;
+}
+
+static int xmp_mkdir(const char *path, mode_t mode) {
+    if (!is_admin) return -EROFS;
+
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+    int res = mkdir(fpath, mode);
+    if (res == -1)
+        return -errno;
+    return 0;
+}
+
+static int xmp_unlink(const char *path) {
+    if (!is_admin) return -EROFS;
+
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+    int res = unlink(fpath);
+    if (res == -1)
+        return -errno;
+    return 0;
+}
+
+static int xmp_rmdir(const char *path) {
+    if (!is_admin) return -EROFS;
+
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+    int res = rmdir(fpath);
+    if (res == -1)
+        return -errno;
+    return 0;
+}
+
+static int xmp_rename(const char *from, const char *to) {
+    if (!is_admin) return -EROFS;
+
+    char ffrom[PATH_MAX];
+    char fto[PATH_MAX];
+    fullpath(ffrom, from);
+    fullpath(fto, to);
+    int res = rename(ffrom, fto);
+    if (res == -1)
+        return -errno;
+    return 0;
 }
 
 static struct fuse_operations xmp_oper = {
@@ -84,15 +451,33 @@ static struct fuse_operations xmp_oper = {
     .readdir = xmp_readdir,
     .open    = xmp_open,
     .read    = xmp_read,
-    .mkdir   = (void*) xmp_writeblock,
-    .unlink  = (void*) xmp_writeblock,
-    .rmdir   = (void*) xmp_writeblock,
-    .rename  = (void*) xmp_writeblock,
-    .write   = (void*) xmp_writeblock,
-    .create  = (void*) xmp_writeblock,
+    .write   = xmp_write,
+    .create  = xmp_create,
+    .mkdir   = xmp_mkdir,
+    .unlink  = xmp_unlink,
+    .rmdir   = xmp_rmdir,
+    .rename  = xmp_rename,
 };
 
 int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+        if (strncmp(argv[i], "--password=", 11) == 0) {
+            const char *pass = argv[i] + 11;
+            if (strcmp(pass, "sisop2025") == 0) {
+                is_admin = 1;
+                printf("[INFO] Akses admin diberikan.\n");
+            } else {
+                printf("[INFO] Password salah! Mode read-only aktif.\n");
+            }
+
+            // Hapus argumen password agar tidak dilempar ke FUSE
+            for (int j = i; j < argc - 1; j++) {
+                argv[j] = argv[j + 1];
+            }
+            argc--;
+            break;
+        }
+    }
+
     return fuse_main(argc, argv, &xmp_oper, NULL);
 }
-
